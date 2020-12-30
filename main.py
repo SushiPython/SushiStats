37bb8a2428ebb1a8b59e821e43bc347a01ecc0c9
Sushi <repl@sushipython.us>
2020-12-30 00:32:50 +0000

    Async rewrite

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template, request, redirect
-import requests
+from quart import Quart, render_template, request, redirect
+import asyncio
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 import random
 import datetime
 import re
-import pymongo
+import motor.motor_asyncio
+import asyncio
+import aiohttp
 
-app = Flask(__name__)
+app = Quart(__name__)
+
+session = None
+'''
+async def get_client_session():
+  global session
+  if session is None:
+    session = await aiohttp.ClientSession()
+  return session
+'''
+
 
 jinja_env = Environment(
     loader=FileSystemLoader(searchpath='templates'),
@@ -40,11 +52,13 @@ color_codes = {
 key = os.getenv('key')
 mongo = os.getenv('mongo')
 
-client = pymongo.MongoClient(f"mongodb+srv://dbUser:{mongo}@cluster0.at4iw.mongodb.net/<dbname>?retryWrites=true&w=majority")
+client = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb+srv://dbUser:{mongo}@cluster0.at4iw.mongodb.net/<dbname>?retryWrites=true&w=majority")
 
-c = client.data.users
+c = client.test.users
 
-
+other_style_codes = {
+    'l': 'font-weight: bold;'
+}
 
 
 def convert_color_codes_to_html(code, symbol, include_raw=False):
@@ -78,14 +92,20 @@ def convert_color_codes_to_html(code, symbol, include_raw=False):
         return output, text_output
     return output
 
-def fCount(user, uuid):
-  if not c.find_one({"username":user}):
-    c.insert_one({
+async def fetch_json(url):
+  async with aiohttp.ClientSession() as session:
+    async with session.get(url) as response:
+      return await response.json()
+'''
+async def storeUser(user, uuid):
+  if not await c.find_one({"username":user}):
+    print(uuid)
+    await c.insert_one({
       "username": user,
       "uuid": uuid
     })
   return
-
+'''
 app.jinja_env.globals[
     'convert_color_codes_to_html'] = convert_color_codes_to_html
 app.jinja_env.globals['round'] = round
@@ -93,40 +113,37 @@ app.jinja_env.globals['datetime'] = datetime
 app.jinja_env.globals['int'] = int
 def su(n):
   print(n)
-  print(type(n))
   return re.sub('&.', '', n)
 app.jinja_env.filters['su'] = su
 app.jinja_env.filters['nu'] = lambda n: '{:,}'.format(n) if n else "0"
 app.jinja_env.filters['fr'] = lambda n: datetime.datetime.fromtimestamp((n-(n%1000))/1000).strftime("%x %X")
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+async def main():
+    return await render_template('index.html')
 
 
 @app.route('/proxy', methods=["GET", "POST"])
-def proxy():
+async def proxy():
     if request.method == "POST":
-        data = request.form
+        data = await request.form
         return redirect(f'/stats/{data["ign"]}', 302)
     if request.method == "GET":
       return redirect('/', 302)
 
 
 @app.route('/stats/<user>', methods=["GET", "POST"])
-def stats(user):
-    if request.method == "GET":
-      uuid = requests.get("https://api.ashcon.app/mojang/v2/user/" + user).json()
-      data = requests.get(f'https://api.slothpixel.me/api/players/{user}?key={key}').json()
-      fCount(user, uuid)
-      return render_template('stats.html', data=data)
+async def stats(user):
+  if request.method == "GET":
+    data = await fetch_json(f'https://api.slothpixel.me/api/players/{user}?key={key}')
+    return await render_template('stats.html', data=data, user=user)
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('error.html', errorCode=404, err="Page Not Found"), 404
+async def page_not_found(e):
+    return await render_template('error.html', errorCode=404, err="Page Not Found"), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
-    return render_template('error.html', errorCode=500, err=""), 500
+async def server_error(e):
+    return await render_template('error.html', errorCode=500, err=""), 500
 
 app.run(host='0.0.0.0', port=8080)
