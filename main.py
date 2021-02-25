a566f0b4555768442b3207eb73ad2408412f0a89
Sushi <repl@sushipython.us>
2021-02-25 00:08:09 +0000

    updated guild stuff

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ app = Quart(__name__, static_folder="static")
 async def static_from_root():
     return await send_from_directory(app.static_folder, request.path[1:])
 
-
-
 jinja_env = Environment(
     loader=FileSystemLoader(searchpath='templates'),
     autoescape=select_autoescape(['html', 'xml']),
@@ -118,15 +116,14 @@ async def proxy():
     if request.method == "GET":
       return redirect('/', 302)
 
-@app.route('/morestats/<user>', methods=["GET", "POST"])
-async def morestats(user):
+@app.route('/stats/<user>', methods=["GET", "POST"])
+async def stats(user):
   if request.method == "GET":
     data = await fetch_json(f'https://api.slothpixel.me/api/players/{user}?key={key}')
     friend_data = await fetch_json(f'https://api.slothpixel.me/api/players/{user}/friends?key={key}')
     guild_data = await fetch_json(f'https://api.slothpixel.me/api/guilds/{user}?key={key}')
     guild_members = 0
-    print(guild_data['guild'])
-    if guild_data['guild'] == None:
+    if guild_data['name'] == None:
       return await render_template('noguild.html', user=user)
     else:
       for b in guild_data["members"]:
@@ -147,13 +144,8 @@ async def morestats(user):
       friends = 0
       for b in friend_data:
           friends += 1
-      return await render_template('stats2.html', data=data, user=user, guild_data=guild_data, guild_members=guild_members, created=created, gm=gm, friends=friends, first_login=first_login, last_login=last_login)
+      return await render_template('stats.html', data=data, user=user, guild_data=guild_data, guild_members=guild_members, created=created, gm=gm, friends=friends, first_login=first_login, last_login=last_login)
 
-@app.route('/stats/<user>', methods=["GET", "POST"])
-async def stats(user):
-  if request.method == "GET":
-    data = requests.get(f'https://api.slothpixel.me/api/players/{user}?key={key}').json()
-    return await render_template('stats.html', data=data, user=user)
 
 @app.errorhandler(404)
 async def page_not_found(e):
